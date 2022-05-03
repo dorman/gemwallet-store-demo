6f4fd032a945e8cc0f7c3efca56d430fba506b93
Eric Dorman <EDorman11@gmail.com>
2022-05-03 05:57:16 +0000

    created category "Pages" and added main store front pages

---
+++ src/components/Pages/store_front.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'reach-dom';
+import './index.css';
+import App from './App';
+import ReportWebVitals from './reactWebVitals';
+
+ReactDOM.render()
+ <React.StrictMode>
+  <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
